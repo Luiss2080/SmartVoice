dd08a10bad4de61782196d4c2c32f7cd3a7507c1
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2025-11-28 15:03:38 -0400

    feat: Initialize Laravel UI package and basic authentication scaffolding.

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ Route::get('/', function () {
     return redirect()->route('login');
 });
 
-Auth::routes();
+// Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
