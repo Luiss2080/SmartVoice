af5d16da9a6533a615d29964d74b2f7d549053a6
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2025-11-28 15:50:19 -0400

    feat: Add Campana and Audio Eloquent models

--- app/Models/Audio.php
+++ app/Models/Audio.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Audio extends Model
 {
-    //
+    protected $table = 'audios';
 }
--- app/Models/Campana.php
+++ app/Models/Campana.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Campana extends Model
 {
-    //
+    protected $table = 'campanas';
 }
