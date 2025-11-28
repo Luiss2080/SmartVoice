39d4da57ba319bfa6653975ab55ec5db94228625
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2025-11-28 15:43:35 -0400

    feat: Add initial web routes for authentication, home, and authenticated sections.

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/campanas', [App\Http\Controllers\CampaignController::class, 'index'])->name('campanas.index');
+    Route::get('/audios', [App\Http\Controllers\AudioController::class, 'index'])->name('audios.index');
+    Route::get('/historial', [App\Http\Controllers\HistoryController::class, 'index'])->name('historial.index');
+    Route::get('/usuarios', [App\Http\Controllers\UserController::class, 'index'])->name('usuarios.index');
+    Route::get('/configuracion', [App\Http\Controllers\SettingsController::class, 'index'])->name('configuracion.index');
+});
